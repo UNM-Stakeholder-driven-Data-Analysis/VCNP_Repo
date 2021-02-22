602962c5fd7fd14c8806f9a4a969a8ab44ca6d22
Brionna <brionna.oconnor@gmail.com>
2021-02-22 12:05:28 -0700

    Set up data wrangle script for temperature

---
+++ R_scripts/01_datawrangle.R
@@ -0,0 +1,10 @@
+#### read me ####
+#Valles Caldera data wrangle temperature datasets
+
+#### libraries ####
+#### load data ####
+#### format date/time ####
+#### plot exploration ####
+#### remove errors ####
+####  datasets ####
+#### save "clean" data ####
